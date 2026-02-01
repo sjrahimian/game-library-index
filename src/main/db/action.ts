ae1b3a90a4326add3463279adfb9b3992c75cfb6
Sama Rahimian <{ID}+{username}@users.noreply.github.com>
2026-02-01 14:28:56 -0500

    Normalizing titles will now decompose the characters (separate the "u" from the "umlaut") and then strip the accents.

--- src/main/db/action.ts
+++ src/main/db/action.ts
@@ -11,6 +11,8 @@ export function normalizeTitle(title: string) {
   return title
     .toLowerCase()
     .trim()
+    .normalize('NFD')
+    .replace(/[\u0300-\u036f]/g, "")
     .replace(/[^a-z0-9\s]/g, '')
     .replace(/\s+/g, '-')
 }
